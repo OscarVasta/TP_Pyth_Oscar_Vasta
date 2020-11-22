db5eaa7d2559ceaa9a0c7006d543d56cdd2296f3
OscarVasta <oscar.vasta@energysapiens.com.ar>
2020-11-22 17:38:56 -0300

    hasta aqui, funciones de objeto a y b funcionando

--- Oscar_Vasta-Guia_Ejercicios_12.1.py
+++ Oscar_Vasta-Guia_Ejercicios_12.1.py
@@ -4,95 +4,100 @@ Oscar_Vasta-Guia_Ejercicios_12.1.
 a) Implementar la clase Intervalo(desde, hasta) que representa un intervalo entre dos
 instantes de tiempo (números enteros expresados en segundos), con la condición desde
 < hasta.
+    **Si el intervalo enviado tubiera un tiempo desde mayor devuelve un assertError alertándolo.**
+
 b) Implementar el método duracion que devuelve la duración en segundos del intervalo.
-    (en caso que el período de teiempo este invertido , calcula la amplitud del período
-     a la inversa para que el resultado de la amplitud del tiempo de un resultado absoluto)
+
 c) Implementar el método interseccion que recibe otro intervalo y devuelve un nuevo intervalo
     resultante de la intersección entre ambos, o lanzar una excepción si la intersección
-    es nula.(el método interseccion devuelve 2 parámetros , el 1º es el intervalode tiempo de
-    intersección y el 2º es un string de aviso si el período se encuentra fuera del entorno o
-    es nulo si los entornos se interceptan)
+    es nula.
+        **Si el período enviado no tiene continuidad con el 1º períod la funcion interseccion() devuelve un ValueError alertándolo.**
+
 d) Implementar el método union que recibe otro intervalo. Si los intervalos no son adyacentes
     ni intersectan, debe lanzar una excepción. En caso contrario devuelve un nuevo
-    intervalo resultante de la unión entre ambos. (el método union devuelve 2 parámetros , el 1º es
-    la suma de los tiempos de los 2 períodos y el 2º es un string de aviso si el período se
-    encuentra fuera del entorno)
+    intervalo resultante de la unión entre ambos.
+        **Si el período enviado no tiene continuidad con el 1º períod la funcion union() devuelve un ValueError alertándolo.**
 """
 # a)
-class intervalo:
+class Intervalo:
+    # Atributos o propiedades
+    desde = 0
+    hasta = 0
+    #interpreto que al poner los atributos en 0 , le estoy indicando que es un número entero, no ?
+
     # constructor
     def __init__(self, desde, hasta):
-        """
-        :param desde: un número entero representado en segundos
-        :param hasta: un número entero representado en segundos mayor a desde
-        """
-        assert desde < hasta, "desde debe ser menor que hasta"
         # atributos de instancia
-        self._desde = desde
-        self._hasta = hasta
+        assert desde < hasta, "desde debe ser menor que hasta"
+        self.desde = desde
+        self.hasta = hasta
 
-        def get_desde(self):
-            return self._desde
 
-        def get_hasta(self):
-            return self._hasta
+
 # b)
-        def duracion(self):
-            return self._hasta - self._desde
+    def duracion(self):
+        return self.hasta - self.desde
 
+"""
 # c)
-    def interseccion(self, otro_intervalo):
+    def interseccion(self):
         # [] ()
         # [(])
         # ([)]
-        i2_desde = otro_intervalo.get_desde()
-        i2_hasta = otro_intervalo.get_hasta()
-        hasta = self.get_hasta()
-        desde = self.get_desde()
-        if hasta > i2_desde and hasta <= i2_hasta:
+
+        if self.hasta1 > self.desde2 and self.hasta1 <= self.hasta2:
             # [()] o ([)]
-            if desde <= i2_desde:
+            if self.desde1 <= self.desde2:
                 # ([)]
-                return Intervalo(i2_desde, hasta)
+                return Intervalo(self.desde2, self.hasta1)
             else:
                 # [()]
-                return Intervalo(desde, hasta)
+                return Intervalo(self.desde1, self.hasta1)
         else:
             # [] ()
             # [(])
-            if desde > i2_desde and desde <= i2_hasta:
+            if self.desde1 > self.desde2 and self.desde1 <= self.hasta2:
                 # [(])
-                return Intervalo(desde, i2_hasta)
+                return Intervalo(self.desde1, self.hasta2)
             else:
                 raise ValueError("No hay interseccion entre intervalos")
 
 # d)
-    def union(self, intervalo_union):
-        def interseccion(self, otro_intervalo):
+    def union(self):
             # [] ()
             # [(])
             # ([)]
-            i3_desde = intervalo_union.get_desde()
-            i3_hasta = intervalo_union.get_hasta()
-            hasta = self.get_hasta()
-            desde = self.get_desde()
-            if hasta > i3_desde and hasta <= i3_hasta:
+            if self.hasta1 > self.desde2 and self.hasta1 <= self.hasta2:
                 # [()] o ([)]
-                if desde <= i3_desde:
+                if self.desde1 <= self.desde2:
                     # ([)]
-                    return intervalo(desde, i3_hasta)
+                    return Intervalo(self.desde1, self.hasta2)
                 else:
                     # [()]
-                    return Intervalo(i3_desde, i3_hasta)
+                    return Intervalo(self.desde2, self.hasta2)
             else:
                 # [] ()
                 # [(])
-                if desde > i2_desde and desde <= i2_hasta:
+                if self.desde1 > self.desde2 and self.desde1 <= self.hasta2:
                     # [(])
-                    return Intervalo(i3_desde, hasta)
+                    return Intervalo(self.desde2, self.hasta1)
                 else:
                     raise ValueError("No hay interseccion entre intervalos")
+"""
 
 
-        
-#------------------ E.O.F. ------------------------------------------------------
+
+
+#----------------------------------------------------------------
+intervalo1=Intervalo(23654, 23789)
+intervalo2=Intervalo(0, 112)
+intervalo3=Intervalo(480, 498)
+intervalos=[intervalo1, intervalo2, intervalo3]
+
+for interv in intervalos:
+        print(interv.duracion())
+
+
+
+
+#------------------------ E.O.F. --------------------------------
