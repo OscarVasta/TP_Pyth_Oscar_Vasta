4c00ea559978f50ed174300ee914bf7a18d38845
OscarVasta <oscar.vasta@energysapiens.com.ar>
2021-01-10 18:35:51 -0300

    Ejercicios 13.1 probado y listo para enviar a revisar

--- Oscar_Vasta-Guia_Ejercicios_13.1.py
+++ Oscar_Vasta-Guia_Ejercicios_13.1.py
@@ -1,7 +1,13 @@
 """
-Oscar_Vasta-Guia_Ejercicios_13.1
+Oscar_Vasta-Guia_Ejercicios_13.1 revisión 1
 Implementar el método __str__ de ListaEnlazada, para que se genere una
 salida legible de lo que contiene la lista, similar a las listas de python.
+** En esta nueva version perfeccione un poco el formato del método __str__
+aunque todavía no logre lo que quiero con la representacion de los puntos
+separadores de miles , los ejemplos que encuentro sobre este tema estan con
+el viejo formato {:,} .format() y los métodos para remmplazar la , por el .
+me parecen demasiado rebuscados , tiene que haber algo mas simple con el
+nuevo formato % f() **
 """
 class Nodo:
     def __init__(self, nombre=None, DNI=None, sig=None):
@@ -9,7 +15,8 @@ class Nodo:
         self.DNI = DNI
         self.sig = sig
     def __str__(self):
-        return "%s %s" %(self.nombre, self.DNI)
+        return "%012s : %8d" %(self.nombre, self.DNI)
+
 class listasimple:
     def __init__(self):
         self.cabeza = None
@@ -30,8 +37,8 @@ class listasimple:
             print (aux)
             aux = aux.sig
 #creamos 2 array con datos iniciales
-nombres = ['Oscar', 'Graciela', 'Mecedes', 'Sara', 'Gise', 'Matias', 'Alexis', 'Ezequiel']
-documentos =[12602584, 12856585, 34569876, 41876543, 29887021, 40987656, 93567876, 39876656]
+nombres = ['Oscar', 'Graciela', 'Mecedes', 'Sara', 'Gise', 'Matias', 'Alexis', 'Ezequiel','Estanislao']
+documentos =[12602584, 12856585, 34569876, 41876543, 29887021, 40987656, 93567876, 39876656, 6986501]
 #Cargamos un array en la lista enlazada
 ls = listasimple()
 for i in range(len(nombres)):
